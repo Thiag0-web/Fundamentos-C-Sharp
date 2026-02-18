eda865405d393020ba2178070e410620fb315d80
Thiag0-web <thiago.fullstacker@gmail.com>
2026-02-18 15:59:40 -0300

    Estudos de entrada em C#

---
+++ Entrada_de_dados/Curso/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo: ");
+            string nome =  Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa? ");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto: ");
+            double preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com seu ultimo nome, idade e altura (mesma linha): ");
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string nomeVet = vet[0];
+            int idadeVet = int.Parse(vet[1]);
+            double alturaVet = double.Parse(vet[2]);
+
+
+                
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco);
+            Console.WriteLine(nomeVet);
+            Console.WriteLine(idadeVet);
+            Console.WriteLine(alturaVet);
+
+        }
+    }
+}
