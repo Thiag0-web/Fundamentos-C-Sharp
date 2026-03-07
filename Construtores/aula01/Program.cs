98bfd4c276e28cff871edb58833765d9f42a9577
Thiag0-web <thiago.fullstacker@gmail.com>
2026-03-06 21:50:25 -0300

    Primeiros passos no C#: Entendendo construtores, new e void

---
+++ Construtores/aula01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Jogo
+{
+    public class Personagem
+    {
+        public string Nome;
+        public string Classe;
+
+        public Personagem(string NomePersonagem, string ClassePersonagem)
+        {
+            this.Nome = NomePersonagem;
+            this.Classe = ClassePersonagem;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Personagem Thiago = new("Thiago", "Mago");
+            Console.WriteLine($"Nome do Personagem: {Thiago.Nome}");
+            Console.WriteLine($"Classe do Personagem: {Thiago.Classe}");
+        }
+    }
+}
+
